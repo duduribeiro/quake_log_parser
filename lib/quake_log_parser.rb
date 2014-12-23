b2d5818b0a3b3ee24be70ccc68413399554d02c7
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2014-12-23 15:23:06 -0200

    Crete the InitGame Analyzer, that will start a game and set the hostname

--- lib/quake_log_parser.rb
+++ lib/quake_log_parser.rb
@@ -1,8 +1,10 @@
 require 'quake_log_parser/version'
 require 'quake_log_parser/executor'
 require 'quake_log_parser/parser'
+require 'quake_log_parser/models/game'
 require 'quake_log_parser/parsers/regex_parser/analyzers/regex_base_analyzer'
 require 'quake_log_parser/parsers/regex_parser/analyzers/command_analyzer'
+require 'quake_log_parser/parsers/regex_parser/analyzers/init_game_analyzer'
 require 'quake_log_parser/parsers/regex_parser/regex_parser'
 
 module QuakeLogParser
---
+++ lib/quake_log_parser/parsers/regex_parser/analyzers/init_game_analyzer.rb
@@ -0,0 +1,16 @@
+module QuakeLogParser
+  module Parsers
+    module Analyzers
+      class InitGameAnalyzer < RegexBaseAnalyzer
+        def initialize(game)
+          super(/.*sv_hostname\\(.*?)\\/, game)
+        end
+
+        def process(line)
+          @game.initialized = true
+          @game.hostname = @regex.match(line).captures.first
+        end
+      end
+    end
+  end
+end
