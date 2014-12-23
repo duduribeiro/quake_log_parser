7534dd6e0c02c985eff4af3609aba870253e5181
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2014-12-23 16:11:20 -0200

    Create the test for start game and for the method to check if the game is started

--- spec/lib/quake_log_parser/models/game_spec.rb
+++ spec/lib/quake_log_parser/models/game_spec.rb
@@ -2,6 +2,27 @@ require 'spec_helper'
 
 RSpec.describe QuakeLogParser::Models::Game do
   let(:game) { described_class.new }
-  
+
   it { is_expected.to respond_to(:start_game, :hostname=) }
+
+  describe "#start_game" do
+    it "will change change the variable @started to true" do
+      game.start_game
+      expect(game.started?).to be_truthy
+    end
+  end
+
+  describe "#started?" do
+    context "with an unstarted game" do
+      it "returns false" do
+        expect(game.started?).to be_falsey
+      end
+    end
+    context "with a started game" do
+      it "returns true" do
+        game.start_game
+        expect(game.started?).to be_truthy
+      end
+    end
+  end
 end
