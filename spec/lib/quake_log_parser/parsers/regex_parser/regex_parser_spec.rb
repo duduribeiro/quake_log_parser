a0ba6635fa029daee7478236b90faa8b143a4117
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2014-12-23 16:19:01 -0200

    Improve spec for regex_parser

--- spec/lib/quake_log_parser/parsers/regex_parser/regex_parser_spec.rb
+++ spec/lib/quake_log_parser/parsers/regex_parser/regex_parser_spec.rb
@@ -12,17 +12,22 @@ RSpec.describe QuakeLogParser::Parsers::RegexParser do
 
   describe "#process_from_command" do
     let(:command_analyzer) { double("command") }
-    let(:init_command) { double("init") }
+    let(:init_command)  { double("init") }
+    let(:another_command) { double("another")  }
     before :each do
       regex_parser.instance_variable_set(:@command_analyzer, command_analyzer)
       regex_parser.instance_variable_set(:@analyzers_by_command, {
-        "init" => init_command
+        "init"    => init_command,
+        "another" => another_command
       })
       allow(command_analyzer).to receive(:process).with("init").and_return("init")
+      allow(command_analyzer).to receive(:process).with("another").and_return("another")
     end
     it "call the process based on line" do
-      expect(init_command).to receive(:process).with("init")
+      expect(init_command).to receive(:process).with("init").once
+      expect(another_command).to receive(:process).with("another").once
       regex_parser.process_from_command("init")
+      regex_parser.process_from_command("another")
     end
   end
 end
