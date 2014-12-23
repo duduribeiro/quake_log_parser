ce4d11d7ea5c68d1d76f55e69e196d50f3cbac6b
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2014-12-23 16:12:43 -0200

    Crete the method process_from_command, that will bypass the process based on the command extracted from the line

--- lib/quake_log_parser/parsers/regex_parser/regex_parser.rb
+++ lib/quake_log_parser/parsers/regex_parser/regex_parser.rb
@@ -3,7 +3,15 @@ module QuakeLogParser
     class RegexParser
       def initialize
         @analyzers_by_command = {
+          "InitGame" => QuakeLogParser::Parsers::Analyzers::InitGameAnalyzer.new(nil),
         }
+        @command_analyzer = QuakeLogParser::Parsers::Analyzers::CommandAnalyzer.new
+      end
+
+      def process_from_command(line)
+        command = @command_analyzer.process(line)
+        @command = @analyzers_by_command[command]
+        @command.process(line) if @command
       end
     end
   end
