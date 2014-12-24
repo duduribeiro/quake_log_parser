8241386a122b8cd11c2113e5bd9894f87445daec
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2014-12-24 21:30:35 -0200

    add the shutdown analyzer in quake_log_parser

--- lib/quake_log_parser.rb
+++ lib/quake_log_parser.rb
@@ -9,6 +9,7 @@ require 'quake_log_parser/parsers/regex_parser/analyzers/command_analyzer'
 require 'quake_log_parser/parsers/regex_parser/analyzers/init_game_analyzer'
 require 'quake_log_parser/parsers/regex_parser/analyzers/client_connect_analyzer'
 require 'quake_log_parser/parsers/regex_parser/analyzers/client_change_info_analyzer'
+require 'quake_log_parser/parsers/regex_parser/analyzers/shutdown_game_analyzer'
 require 'quake_log_parser/parsers/regex_parser/analyzers/kill_analyzer'
 require 'quake_log_parser/parsers/regex_parser/regex_parser'
 
