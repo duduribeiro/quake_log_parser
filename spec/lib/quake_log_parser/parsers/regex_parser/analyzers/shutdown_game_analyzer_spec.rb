56434679f5cd13f09f2380456f9388eea7e200e0
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2014-12-24 21:33:19 -0200

    Only shutdown a game if it has been started

--- spec/lib/quake_log_parser/parsers/regex_parser/analyzers/shutdown_game_analyzer_spec.rb
+++ spec/lib/quake_log_parser/parsers/regex_parser/analyzers/shutdown_game_analyzer_spec.rb
@@ -2,12 +2,41 @@ require 'spec_helper'
 
 RSpec.describe QuakeLogParser::Parsers::Analyzers::ShutdownGameAnalyzer do
   let(:game) { double("game", shutdown:true) }
-  let(:analyzer) { described_class.new(game) }
+  let(:analyzer) { described_class.new }
   describe "#process" do
-    it "will finalize the game" do
-      line = '20:37 ShutdownGame:'
-      expect(game).to receive(:shutdown)
-      analyzer.process(line)
+    context "with the shutdown command" do
+      let(:line) { '20:37 ShutdownGame:' }
+      context "with a started game" do
+        it "will finalize the game" do
+          allow(game).to receive(:started?).and_return(true)
+          expect(game).to receive(:shutdown)
+          analyzer.process(line, game)
+        end
+      end
+      context "withe an unstarted game" do
+        it "will not finalize the game" do
+          allow(game).to receive(:started?).and_return(false)
+          expect(game).to_not receive(:shutdown)
+          analyzer.process(line, game)
+        end
+      end
+    end
+    context "with the dot line command" do
+      let(:line) { '14:11 ------------------------------------------------------------' }
+      context "with a started game" do
+        it "will finalize the game" do
+          allow(game).to receive(:started?).and_return(true)
+          expect(game).to receive(:shutdown)
+          analyzer.process(line, game)
+        end
+      end
+      context "withe an unstarted game" do
+        it "will not finalize the game" do
+          allow(game).to receive(:started?).and_return(false)
+          expect(game).to_not receive(:shutdown)
+          analyzer.process(line, game)
+        end
+      end
     end
   end
 end
