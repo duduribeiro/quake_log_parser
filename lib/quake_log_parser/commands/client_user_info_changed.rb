66dccab208066f3d7d7c081ae88f238138a54b17
Cadu Ribeiro <mail@cadu.dev>
2022-05-11 15:36:08 -0300

    Change ClientUserInfoChanged to use the id as integer
    This will fix the bug where we tried to find a player id as string but
    it was stored as int.

--- lib/quake_log_parser/commands/client_user_info_changed.rb
+++ lib/quake_log_parser/commands/client_user_info_changed.rb
@@ -1,7 +1,7 @@
 module QuakeLogParser::Commands
   class ClientUserInfoChanged
     def self.process(game, player_id, new_name)
-      player = game.players[player_id]
+      player = game.players[player_id.to_i]
       player&.name = new_name
     end
   end
