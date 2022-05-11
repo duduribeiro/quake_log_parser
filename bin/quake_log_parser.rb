dc444502cb6093195142d3a502f917c797f1b1b5
Cadu Ribeiro <mail@cadu.dev>
2022-05-11 19:49:50 +0000

    Change the parser to process each command and generate the reports
    This commit changes the parser to process each line executing the
    command returned from the parse strategy. It also generates the
    report and returns it to the client.
    
    The bin/quake_log_parser uses these reports generated from the parser
    to print the result on the CLI.

--- bin/quake_log_parser.rb
+++ bin/quake_log_parser.rb
@@ -2,8 +2,13 @@
 $LOAD_PATH << File.expand_path('../../lib', __FILE__)
 
 require "quake_log_parser"
+require "json"
 
 puts "-- Quake Log Parser"
 
 filename = ARGV.first
-QuakeLogParser::Parser.new(filename).parse
+reports = QuakeLogParser::Parser.new(filename).parse_reports
+
+reports.each do |report|
+  puts JSON.pretty_generate(report.as_json)
+end
--- lib/quake_log_parser/parser.rb
+++ lib/quake_log_parser/parser.rb
@@ -1,4 +1,8 @@
 require "quake_log_parser/parser_strategies/regex"
+require "quake_log_parser/models/game"
+require "quake_log_parser/models/reports/game"
+require "quake_log_parser/models/reports/general_ranking"
+
 module QuakeLogParser
   class Parser
     def initialize(filename)
@@ -6,15 +10,44 @@ module QuakeLogParser
 
       @file = File.new(filename, "r")
       @parse_strategy = ParserStrategies::Regex.new # only implemented Regex Strategy but in future can have implementations with other parsers like Treetop, Citrus or Parslet.
+      @games = []
     end
 
-    def parse
+    def parse_reports
+      reports = []
       while (line = @file.gets)
         cmd, args = @parse_strategy.get_command_and_args(line)
-        next unless cmd
+        cmd&.process(current_game, *args)
 
-        p cmd
+        reports << save_and_generate_report_for_current_game if current_game.finalized?
       end
+      reports << QuakeLogParser::Models::Reports::GeneralRanking.new(@games).generate
+      reports
+    end
+
+    private
+
+    def current_game
+      @current_game || new_game
+    end
+
+    def new_game
+      @current_game = QuakeLogParser::Models::Game.new(next_round)
+    end
+
+    def next_round
+      @round = current_round + 1
+    end
+
+    def current_round
+      @round ||= 0
+    end
+
+    def save_and_generate_report_for_current_game
+      @games << current_game
+      report = QuakeLogParser::Models::Reports::Game.new(current_game).generate
+      new_game
+      report
     end
   end
 end
